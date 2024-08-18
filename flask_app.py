9cd5cab4813b82b9ff6134593c83a0109503bd62
hy20191108 <57515337+hy20191108@users.noreply.github.com>
2024-08-18 11:22:19 +0900

    Update flask wrapper

--- flask_app.py
+++ flask_app.py
@@ -4,10 +4,10 @@ import numpy as np
 import tensorflow as tf
 from flask import Flask, request
 
-import _code2seq
-from config import Config
-from interactive_predict import InteractivePredictor
-from model import Model
+from code2seq import _code2seq
+from code2seq.config import Config
+from code2seq.interactive_predict import InteractivePredictor
+from code2seq.model import Model
 
 app = Flask(__name__)
 
@@ -20,7 +20,7 @@ model = Model(config)
 predictor = InteractivePredictor(config, model)
 
 
-@app.route("/api/data", methods=["POST"])
+@app.route("/api/code2seq", methods=["POST"])
 def process_data():
     source_code = request.json["source_code"]
     print(source_code)
@@ -31,4 +31,4 @@ def process_data():
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
