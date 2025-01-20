437a9c5fd115b554ba6a6dffa6286602a2f42e91
Haruhiko Yoshioka <zwwp9976@gmail.com>
2025-01-20 22:08:42 +0900

    Refactor flask app code

--- flask_app.py
+++ flask_app.py
@@ -1,23 +1,20 @@
 import sys
 from pathlib import Path
 
-# プロジェクトのルートディレクトリを sys.path に追加
-# sharedをimportするために必要
+# Add project root to sys.path for shared imports
 project_root = Path(__file__).resolve().parents[1]
 if str(project_root) not in sys.path:
     sys.path.insert(0, str(project_root))
 
 # Setup logging
 from shared.logger_manager import LoggerManager
+
 base_dir = Path(__file__).resolve().parents[2]
 LoggerManager.setup_logging("code2seq_logging_config.yaml", base_dir)
 logger = LoggerManager("code2seq_flask_app")
 logger.info("code2seq started")
 
-
 import pickle
-from pathlib import Path
-
 import numpy as np
 import tensorflow as tf
 from flask import Flask, jsonify, request
@@ -29,12 +26,8 @@ from model import Model
 # Initialize Flask app
 app = Flask(__name__)
 
-
-# Generate predictor
 def initialize_predictor():
-    from interactive_predict import (
-        InteractivePredictor,  # ロギングしているので、ここでインポートする
-    )
+    from interactive_predict import InteractivePredictor
 
     args = _code2seq.get_args()
     logger.info(f"Arguments: {args}")
@@ -44,9 +37,7 @@ def initialize_predictor():
     model = Model(config)
     return InteractivePredictor(config, model)
 
-
 predictor = initialize_predictor()
-
 
 @app.route("/api/code2seq", methods=["POST"])
 def process_data():
@@ -62,7 +53,6 @@ def process_data():
     except Exception as e:
         logger.error(f"Error processing data: {e}")
         return jsonify({"error": "Internal server error"}), 500
-
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
